a61f9ce7b65244c2f472488171e285232c5c12a7
Bruno Marques <marques.bs@outlook.com.br>
2022-05-25 21:08:04 -0300

    Add files via upload

---
+++ SECAO-1/Prova/ex1/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o
+código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago.*/ 
+using System;
+using System.Globalization; //Para conseguir digitar ponto ao invés de vírgula nos valores.
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o código da peça: ");
+            int cod1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite a quantidade de peças: ");
+            int qnt1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor unitário da peça: ");
+            double valor1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); //CultureInfo.InvariantCulture serve para aceitar o ponto no lugar da vírgula.
+
+            Console.Write("Digite o código da peça: ");
+            int cod2 = int.Parse(Console.ReadLine());
+            Console.Write("Digite a quantidade de peças: ");
+            int qnt2 = int.Parse(Console.ReadLine());
+            Console.Write("Digite o valor unitário da peça: ");
+            double valor2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double total = (qnt1 * valor1) + (qnt2 * valor2);
+            
+            Console.WriteLine("O valor total a ser pago é de: {0:F2}", total);
+        }
+    }
+}
---
+++ SECAO-1/Prova/ex2/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+casas decimais conforme exemplos.
+Fórmula: area = π * raio (ao quadrado)
+Considere o valor de π = 3.14159*/
+
+using System;
+using System.Globalization;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor do raio de um círculo: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double area = 3.14159 * Math.Pow(raio, 2);
+            Console.WriteLine($"{area.ToString("F4", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
