028e97d8d7e52175e02bc7bcb7bc55a4b7e1b817
Bruno Marques <marques.bs@outlook.com.br>
2022-06-01 21:43:27 -0300

    Adequação de pastas e novos exercícios

---
+++ SECAO-3/ex1_estrutura_sequencial/Program.cs
@@ -0,0 +1,15 @@
+﻿//Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma mensagem explicativa, conforme exemplos.
+using System;
+
+namespace ex1_estrutura_sequencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"SOMA = {n1 + n2}");
+        }
+    }
+}
