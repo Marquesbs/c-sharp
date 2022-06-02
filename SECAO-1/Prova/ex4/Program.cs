0e7d5de907a7414d0ea50bbd3f5347c12d527a60
Bruno Marques <marques.bs@outlook.com.br>
2022-06-01 21:26:47 -0300

    Atualizando repositório

---
+++ SECAO-1/Prova/ex4/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Ler os valores dos três coeficientes "a", "b" e "c" de uma equação do segundo grau. Em seguida, 
+mostrar os valores das raízes da equação, conforme exemplos, usando a fórmula de Baskara (veja abaixo). 
+Se a equação não possuir raízes (o valor de "a" não pode ser zero, e o valor de "delta" não pode ser negativo), mostrar uma mensagem "Impossivel calcular".*/
+using System;
+
+namespace ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o valor de A: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Entre com o valor de A: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Entre com o valor de A: ");
+            double c = double.Parse(Console.ReadLine());
+            
+            double delta = Math.Pow(b, 2) - (4 * a * c);//funcão de potênciação.
+
+            double x1 = (-b + Math.Sqrt(delta))/(2 * a);//função de raiz quadrada.
+            double x2 = (-b - Math.Sqrt(delta))/(2 * a);
+
+            if(a == 0.0 || delta < 0.0) //se a for igual a zero ou delta menor que zero, então mostrar na tela impossível calcular.
+            {
+                System.Console.WriteLine("Impossível Calcular");
+            }
+
+            else
+            {
+                System.Console.WriteLine(x1.ToString("F5"));
+                System.Console.WriteLine(x2.ToString("F5"));
+            }
+        }
+    }
+}
---
+++ SECAO-3/ex_fixacao_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ex_fixacao_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem a sua casa?: ");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto: ");
+            float preco = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre com seu último nome, idade e altura: (mesma linha)");
+            string[] vetor = Console.ReadLine().Split(' ');
+            
+            System.Console.WriteLine(nome);
+            System.Console.WriteLine(quartos);
+            System.Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            System.Console.WriteLine(vetor[0]);
+            System.Console.WriteLine(vetor[1]);
+            System.Console.WriteLine(vetor[2]);
+        }
+    }
+}
