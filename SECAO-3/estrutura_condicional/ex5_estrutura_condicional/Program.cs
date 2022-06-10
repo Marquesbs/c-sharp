7fa726382ddb1fefe432b3f79b90f2cf8fe02e0a
Bruno Marques <marques.bs@outlook.com.br>
2022-06-09 22:32:21 -0300

    Resolução do Ex 5 de estrutura condicional

---
+++ SECAO-3/estrutura_condicional/ex5_estrutura_condicional/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ex5_estrutura_condicional
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            int codigo = int.Parse(Console.ReadLine());
+            int quantidade = int.Parse(Console.ReadLine());
+            double valor = 0.0f;
+
+            if (codigo == 1)
+            {
+                valor = (double) quantidade * 4.0f;
+                Console.WriteLine($"Total: R$ " + valor.ToString("F2"));
+            }
+            if (codigo == 2)
+            {
+                valor = (double) quantidade * 4.50f;
+                Console.WriteLine($"Total: R$ " + valor.ToString("F2"));
+            }
+            if (codigo == 3)
+            {
+                valor = (double) quantidade * 5.0f;
+                Console.WriteLine($"Total: R$ " + valor.ToString("F2"));
+            }
+            if (codigo == 4)
+            {
+                valor = (double) quantidade * 2.0f;
+                Console.WriteLine($"Total: R$ " + valor.ToString("F2"));
+            }
+            if (codigo == 5)
+            {
+                valor = (double) quantidade * 1.0f;
+                Console.WriteLine($"Total: R$ " + valor.ToString("F2"));
+            }
+        }
+    }
+}
