3c820d065cb210fb65d0c6f1b54837549f20bd6e
Bruno Marques <marques.bs@outlook.com.br>
2022-06-18 15:06:40 -0300

    Exercícios estrutura While

---
+++ SECAO-3/estrutura_while/ex1_estrutura_while/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
+incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser
+impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.*/
+using System;
+
+namespace ex1_estrutura_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha = 0;
+            while(senha != ((int)2002))
+            {
+                senha = int.Parse(Console.ReadLine());
+                if (senha != (int)2002)
+                {
+                    System.Console.WriteLine("Senha Inválida");
+                }
+                else
+                {
+                    System.Console.WriteLine("Acesso Permitido");
+                }
+            }            
+        }
+    }
+}
---
+++ SECAO-3/estrutura_while/ex2_estrutura_while/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema
+cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
+menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).*/
+using System;
+
+namespace ex2_estrutura_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            int valor_X = int.Parse(valores[0]);
+            int valor_Y = int.Parse(valores[1]);;
+            while(valor_X != 0 && valor_Y != 0)
+            {
+                if(valor_X > 0 && valor_Y > 0)
+                {
+                    System.Console.WriteLine("primeiro");
+                }
+                else if(valor_X > 0 && valor_Y < 0)
+                {
+                    System.Console.WriteLine("quarto");
+                }
+                else if(valor_X < 0 && valor_Y > 0)
+                {
+                    System.Console.WriteLine("segundo");
+                }
+                else
+                {
+                    System.Console.WriteLine("terceiro");
+                }
+                valores = Console.ReadLine().Split(' ');
+                valor_X = int.Parse(valores[0]);
+                valor_Y = int.Parse(valores[1]);
+            }
+        }
+    }
+}
