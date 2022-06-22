a6c8e0a278aea5f874a6097276cc8e215fa45b27
Bruno Marques <marques.bs@outlook.com.br>
2022-06-22 19:59:12 -0300

    Realização de dois exercícios de estrutura for

---
+++ SECAO-3/estrutura_for/ex2_estrutura_for/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida.
+Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando
+essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, e "out" para fora do intervalo).*/
+using System;
+
+namespace ex2_estrutura_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = int.Parse(Console.ReadLine());
+            int dentro = 0, fora = 0, valor = 0;
+
+            for(int i = 0; i < numero; i++)
+            {
+                valor = int.Parse(Console.ReadLine());
+                if(valor >= 10 && valor<= 20)
+                {
+                    dentro++;
+                }
+                else
+                {
+                    fora++;
+                }
+            }
+            System.Console.WriteLine(dentro + " In");
+            System.Console.WriteLine(fora + " Out");
+        }
+    }
+}
