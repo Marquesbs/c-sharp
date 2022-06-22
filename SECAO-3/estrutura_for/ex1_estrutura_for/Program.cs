c4489b14462ded8b348d8a72beed29e783c21fe1
Bruno Marques <marques.bs@outlook.com.br>
2022-06-21 22:29:53 -0300

    upload de exerrcício de estrutura for

---
+++ SECAO-3/estrutura_for/ex1_estrutura_for/Program.cs
@@ -0,0 +1,21 @@
+﻿//Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.
+using System;
+
+namespace ex1_estrutura_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            
+            for (int valor = 1; valor <= x; valor++)
+            {
+                if (valor % 2 != 0)
+                {
+                    Console.WriteLine(valor);
+                }
+            }
+        }
+    }
+}
