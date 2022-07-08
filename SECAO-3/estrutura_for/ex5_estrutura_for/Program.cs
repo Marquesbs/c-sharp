55f23228bfe18d6676a13b8a1176dd463873e765
Bruno Marques <marques.bs@outlook.com.br>
2022-07-07 22:10:14 -0300

    resolução ex5 estrutura for

---
+++ SECAO-3/estrutura_for/ex5_estrutura_for/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Ler um valor N. Calcular e escrever seu respectivo fatorial. Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+Lembrando que, por definição, fatorial de 0 é 1.*/
+using System;
+
+namespace ex5_estrutura_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int fatorial = N;
+            if (N != 0)
+                {
+                    for(int c = 1;c < N; c++)
+                    {
+                        fatorial = fatorial * c;
+                    }
+                    System.Console.WriteLine(fatorial);
+                }
+            else
+            {
+                System.Console.WriteLine("1");
+            }
+        }
+    }
+}
---
+++ SECAO-3/estrutura_for/ex6_estrutura_for/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ex6_estrutura_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
