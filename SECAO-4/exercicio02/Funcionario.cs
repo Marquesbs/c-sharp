aeed8727b954ebf54db4b2eb215cb4536c7f66ce
Bruno Marques <marques.bs@outlook.com.br>
2022-07-09 00:28:15 -0300

    resol. exercício 2, com uso de função e classe

---
+++ SECAO-4/exercicio02/Funcionario.cs
@@ -0,0 +1,8 @@
+namespace exercicio02
+{
+    public class Funcionario
+    {
+        public string nome;
+        public double salario;
+    }
+}
---
+++ SECAO-4/exercicio02/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário
+médio dos funcionários.
+Exemplo:
+Dados do primeiro funcionário:
+Nome: Carlos Silva
+Salário: 6300.00
+Dados do segundo funcionário:
+Nome: Ana Marques
+Salário: 6700.00
+Salário médio = 6500.00*/
+
+using System;
+using System.Globalization;
+
+namespace exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func1, func2;
+            func1 = new Funcionario();
+            func2 = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            func1.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            func1.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("\nDados do segundo funcionário:");
+            Console.Write("Nome: ");
+            func2.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            func2.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double salarioMedio = calculoMedia(func1.salario, func2.salario);
+            Console.WriteLine("Salário médio = " + salarioMedio.ToString("F2", CultureInfo.InvariantCulture));
+        }
+        static double calculoMedia(double salario1, double salario2)
+        {
+            double Media = (salario1 + salario2)/2;
+            return Media;
+        }
+    }
+}
